5b06fe7ae6a82fae27d689839fce9f8bdc650218
Jamie Young <jamielove891@hanmail.net>
2021-03-24 21:44:48 +0900

    feat: ADD contexts

--- ts-context/src/App.tsx
+++ ts-context/src/App.tsx
@@ -1,13 +1,14 @@
 import React from "react";
 import TodoForm from "./components/TodoForm";
 import TodoList from "./components/TodoList";
+import { TodoContextProvider } from "./contexts/TodosContext";
 
 const App = () => {
   return (
-    <>
+    <TodoContextProvider>
       <TodoForm />
       <TodoList />
-    </>
+    </TodoContextProvider>
   );
 };
 
