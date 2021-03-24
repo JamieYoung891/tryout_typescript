098a9dbe2a27c5b499212d48aab4e8228e9ff4b7
Jamie Young <jamielove891@hanmail.net>
2021-03-24 18:22:19 +0900

    feat: REFACTOR Counter (useReducer)

--- ts-react/src/components/Counter.tsx
+++ ts-react/src/components/Counter.tsx
@@ -1,9 +1,22 @@
-import React, { useState } from "react";
+import React, { useReducer, useState } from "react";
+
+type CounterAction = { type: "INCREASE" } | { type: "DECREASE" };
+
+const counterReducer = (state: number, action: CounterAction): number => {
+  switch (action.type) {
+    case "INCREASE":
+      return state + 1;
+    case "DECREASE":
+      return state - 1;
+    default:
+      throw new Error("Unhandled action");
+  }
+};
 
 export const Counter = () => {
-  const [count, setCount] = useState<number>(0); // generics can be omitted
-  const handleIncrease = () => setCount(count + 1);
-  const handleDecrease = () => setCount(count - 1);
+  const [count, dispatch] = useReducer(counterReducer, 0);
+  const handleIncrease = () => dispatch({ type: "INCREASE" });
+  const handleDecrease = () => dispatch({ type: "DECREASE" });
 
   return (
     <div>
